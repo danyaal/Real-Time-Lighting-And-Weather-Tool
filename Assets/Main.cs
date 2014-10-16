ea7b29ac9f74d8d2bccea30d979de0550df5a749
Danyaal Rangwala <thedanyaal@gmail.com>
2014-10-15 17:41:25 -0700

    demo set up

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -11,4 +11,21 @@ public class Main : MonoBehaviour {
 		Instantiate (RealTimeLightingPrefab);
 	}
 
+	void Update() {
+		if(Input.GetKeyDown("0")) {
+			GameObject rtlp = GameObject.FindGameObjectWithTag("lightsource");
+			rtlp.GetComponent<RealTimeLightingScript>().DebugRain = false;
+			rtlp.GetComponent<RealTimeLightingScript>().DebugSnow = false;
+		}
+		if(Input.GetKeyDown("1")) {
+			GameObject rtlp = GameObject.FindGameObjectWithTag("lightsource");
+			rtlp.GetComponent<RealTimeLightingScript>().DebugRain = true;
+			rtlp.GetComponent<RealTimeLightingScript>().DebugSnow = false;
+		} else if(Input.GetKeyDown("2")) {
+			GameObject rtlp = GameObject.FindGameObjectWithTag("lightsource");
+			rtlp.GetComponent<RealTimeLightingScript>().DebugRain = false;
+			rtlp.GetComponent<RealTimeLightingScript>().DebugSnow = true;
+		}
+	}
+
 }
